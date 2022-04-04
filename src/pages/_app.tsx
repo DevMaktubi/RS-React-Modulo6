a4af09003267f1b6e18243ac38c833235f40f901
devmaktubi <brenoluiz1@gmail.com>
2022-04-04 09:30:30 -0300

    feat(mirage): Added mirageJS and React Query services to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,30 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
-      <ChakraProvider theme={theme}>
-        <SidebarDrawerProvider>
-          <Component {...pageProps} />
-        </SidebarDrawerProvider>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <SidebarDrawerProvider>
+            <Component {...pageProps} />
+          </SidebarDrawerProvider>
+        </ChakraProvider>
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-unresolved */
+import { createServer, Factory, Model } from 'miragejs';
+import faker from 'faker';
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+    seeds(server) {
+      server.createList('user', 10);
+    },
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
